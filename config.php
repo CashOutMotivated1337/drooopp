99ab8c7985071c79b935c9e651a596ae702118dd
CashOutMotivated1337 <139492588+CashOutMotivated1337@users.noreply.github.com>
2023-11-28 07:59:00 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $conf["login"] = "god";
+    $conf["password"] = "00eb9dfd7cb74da2f5c3ab9d3e13a022";
+    $conf["dbhost"] = "localhost";
+    $conf["dbname"] = "onedrive";
+    $conf["dbuser"] = "onedrive";
+    $conf["dbpass"] = "AuschwitzN732015";
+?>
